d37eeb288369d158ee0e655ed916fea1d8270385
Ole Jørgen Skogstad <skogstad@softis.net>
2021-03-07 22:29:18 +0100

    Initial Commit

---
+++ Shared/TimerLooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerLooperApp.swift
+//  Shared
+//
+//  Created by Ole Jørgen Skogstad on 07/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerLooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
