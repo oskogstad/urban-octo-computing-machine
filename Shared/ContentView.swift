f780330b140fbca5baffa0f6f101d942d6fa2f39
Ole Jørgen Skogstad <skogstad@softis.net>
2021-03-07 23:14:57 +0100

    Enable hot reload for AppCode/Injection

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, injection reload!")
+                .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+class ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
+    #if DEBUG
+    @objc class func injected() {
+        UIApplication.shared.windows.first?.rootViewController =
+                UIHostingController(rootView: ContentView())
+    }
+    #endif
 }
